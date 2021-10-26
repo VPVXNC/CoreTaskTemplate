481f8b693b1afda292e7a79d7ad4ca29438eae79
VPVXNC <dobro240chan@gmail.com>
2021-10-26 21:01:52 +0300

    Fixed

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,16 +7,18 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
+
         UserService userService = new UserServiceImpl();
 
-        //userService.dropUsersTable();
+        userService.createUsersTable();
 
-        //userService.createUsersTable();
-        //userService.saveUser("Ivan", "Cherepitsa", Byte.parseByte("25"));
-        //userService.saveUser("Artem", "Chetvernin", Byte.parseByte("25"));
-        //userService.saveUser("Olga", "Kolobkova", Byte.parseByte("24"));
-        //userService.saveUser("Alber", "Albertov", Byte.parseByte("23"));
-        //System.out.println(userService.getAllUsers());
-        //userService.dropUsersTable();
+        userService.saveUser("Ivan", "Cherepitsa", Byte.parseByte("25"));
+        userService.saveUser("Artem", "Chetvernin", Byte.parseByte("25"));
+        userService.saveUser("Olga", "Kolobkova", Byte.parseByte("24"));
+        userService.saveUser("Alber", "Albertov", Byte.parseByte("23"));
+
+        System.out.println(userService.getAllUsers());
+
+        userService.dropUsersTable();
     }
 }
